513d5feb5daf9001b495a95faa4d2714ff74edef
Albert Kharisov <ah@bright-box.com>
2017-10-30 00:23:15 +0300

    exc_1_4_4: typeid, check is the same objects

--- second/exc_1_4_4/main.cpp
+++ second/exc_1_4_4/main.cpp
@@ -4,13 +4,39 @@ using namespace std;
 
 #include <typeinfo>
 
+struct Expression
+{
+    virtual ~Expression(){};
+    int a;
+};
+
+struct number1 : Expression
+{
+    int b;
+};
+
+struct number2 : Expression
+{
+    double c;
+};
+
 bool check_equals(Expression const *left, Expression const *right)
 {
-    // put your code here
+    return typeid(*left) == typeid(*right);
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    number1 * n1 = new(number1);
+    number2 * n2 = new(number2);
+
+    cout << "number1 == number2 ? " << check_equals(n1, n2) << endl;
+    cout << "number1 == number1 ? " << check_equals(n1, n1) << endl;
+    cout << "number2 == number2 ? " << check_equals(n2, n2) << endl;
+    cout << "number2 == number1 ? " << check_equals(n2, n1) << endl;
+
     return 0;
 }
+
+
+
