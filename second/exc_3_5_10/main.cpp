425a922a62429cd7a177cbd59c4d7f307e929e31
Albert Kharisov <ah@bright-box.com>
2017-11-19 03:20:22 +0300

    exc_3_5_10: question: find_if, remove_copy_if... ElementN.

---
+++ second/exc_3_5_10/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template<class Iterator, class Pred>
+Iterator remove_if1(Iterator p, Iterator q, Pred pred)
+{
+    Iterator s = std::find_if(p, q, pred);
+    if (s == q)
+        return q;
+
+    Iterator out = s;
+    ++s;
+    return remove_copy_if(s, q, out, pred);
+}
+
+struct ElementN
+{
+    explicit ElementN(size_t n)
+        : n(n), i(0)
+    {}
+
+    template<class T>
+    bool operator()(T const& t) { return (i++ == n); }
+
+    size_t n;
+    size_t i;
+};
+
+int main()
+{
+    std::vector<int> v = { 0,1,2,3,4,5,6,7,8,9,10,11,12 };
+
+    v.erase(remove_if1(v.begin(), v.end(), ElementN(3)), v.end());
+
+    for (int i: v)
+        std::cout << i << ' ';
+
+    return 0;
+}
+
